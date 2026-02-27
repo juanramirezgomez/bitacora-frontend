f4d521d6e0ad9924490fd51c5feb21061bbec2a7
juanramirezgomez <jota.raaamirez@gmail.com>
2026-02-27 03:45:40 -0300

    Fix Ionic standalone production rendering

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, RouteReuseStrategy } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { IonicStorageModule } from '@ionic/storage-angular';
@@ -9,6 +10,7 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
 import { addIcons } from 'ionicons';
 import { logOutOutline, eyeOutline, eyeOffOutline } from 'ionicons/icons';
 
@@ -18,18 +20,23 @@ addIcons({
   'eye-off-outline': eyeOffOutline,
 });
 
-
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideIonicAngular(),
+    provideIonicAngular({
+      mode: 'md'
+    }),
+
     provideRouter(routes),
     provideHttpClient(),
 
-    // ✅ Storage correcto en standalone
+    // 🔥 IMPORTANTE para que Ionic renderice correctamente en producción
+    provideAnimations(),
+
+    // Storage standalone
     importProvidersFrom(IonicStorageModule.forRoot()),
 
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
